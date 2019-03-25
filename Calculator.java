ce65d1994c36f208785068fd04cffe5ee9f01584
rleongPd5 <rleong00@stuy.edu>
2019-03-25 11:41:10 -0400

    Added notes and a helper tokenize method header

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,17 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){}
+    public static double eval(String s){
+
+    }
+
+    //helper method to divide up the string
+    public static MyDeque tokenize(String s){
+
+    }
 }
+/*
+1. Convert your string into tokens. (A list of values and operators)
+1b. Test this by printing each one!
+2. Instead of printing each one, decide what to do with them...
+*/
