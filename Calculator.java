6ce66a173b963aaf833c67a57c7887a03591269f
rleongPd5 <rleong00@stuy.edu>
2019-03-25 11:48:26 -0400

    STarted eval and added new helper method header

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,24 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
+      MyDeque<int> = new MyDeque(s.length() / 2 + 1); //create a new MyDeque
+      for (int i = 0; i < s.length(); i++){ //loop through the string
+        if (charAt())
+      }
+    }
+
+    /* Notes on deque
+    Add: (push / en-queue)
+    Get: return but NOT remove the element. (peek)
+    Remove: return AND remove the element. (pop / de-queue)
+    */
+    //helper method to divide up the string
+    public static MyDeque tokenize(String s){
 
     }
 
-    //helper method to divide up the string
-    public static MyDeque tokenize(String s){
+    //helper method for determining whether or not something is an operator 
+    public static boolean isOperator(String s){
 
     }
 }
