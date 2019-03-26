6a20df043d7432b9a3fdc917729a49b45ab2a05d
rleongPd5 <rleong00@stuy.edu>
2019-03-26 11:33:24 -0400

    Compiles, realized that I need scanner b/c spaces are also characters :(

--- Calculator.java
+++ Calculator.java
@@ -3,18 +3,20 @@ public class Calculator{
      *Assume valid postfix notation, separated by spaces.
      */
     public static double eval(String s){
-      MyDeque<int> m = new MyDeque(s.length() / 2 + 1); //create a new MyDeque
+      MyDeque<Double> m = new MyDeque<Double>(s.length() / 2 + 1); //create a new MyDeque
       for (int i = 0; i < s.length(); i++){ //loop through the string
-        if (isOperator(charAt(i))){
-          Double val2 = m.removeLast();
-          Double val1 = m.removeLast();
-          m.addLast(val1+val2);
+        if (isOperator(s.charAt(i))){
+          double val2 = m.removeLast();
+          double val1 = m.removeLast();
+          m.addLast(operate(s.charAt(i), val1, val2));
+          //Print the token
+          System.out.println("Token: " + val1 + " " + val2 + " "+ s.charAt(i));
         }
         else{
-          m.addLast(charAt(i)+0)); //makes this an integer
+          m.addLast(s.charAt(i)+0.0); //makes this a double
         }
-        return m.getLast();
       }
+      return m.getLast();
     }
 
     /* Notes on deque
@@ -29,7 +31,62 @@ public class Calculator{
 
     //helper method for determining whether or not something is an operator
     public static boolean isOperator(char c){
-      return (c == '+' ||c == '-' || c==' *' || c == '/' || c == '%');
+      return (c == '+' ||c == '-' || c== '*' || c == '/' || c == '%');
+    }
+
+    public static double operate(char operater, double i1, double i2){
+      if (operater == '+'){
+        return i1 + i2;
+      }
+      else if (operater == '-'){
+        return i1 - i2;
+      }
+      else if (operater == '*'){
+        return i1 * i2;
+      }
+      else if (operater == '/'){
+        return i1 / i2;
+      }
+      else if (operater == '%'){
+        return i1 % i2;
+      }
+      return 0; //dummy
+    }
+
+    //testing
+
+    public static void main(String[] args){
+      //Test strings
+      String[] tests = new String[]{
+        "3 2 + ",
+        "4 3 - 9 * ",
+        "8 9 60 10 1 * / 5 - % + ",
+        // 8 9 60 10 / 5 - % +
+        // 8 9 6 5 - % +
+        // 8 9 1 % +
+        // 8 0 +
+        // 8
+        "5 9 * 10 - 8 + 2 * "
+        // 45 10 - 8 + 2 *
+        // 35 8 + 2 *
+        // 41 2 *
+        // 82
+      };
+      int[] answers = new int[]{
+        5,
+        9,
+        8,
+        82
+      };
+      for (int i = 0; i < tests.length; i++){
+        if (eval(tests[i])==answers[i]){
+          System.out.println("Test PASSED :D");
+        }
+        else{
+          System.out.println("Test FAILED :(");
+        }
+        System.out.println(tests[i] + ": "+answers[i] + "\n------------------------------------\n");
+      }
     }
 }
 /*
